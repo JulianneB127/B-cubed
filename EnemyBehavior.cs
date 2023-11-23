dc886fb6b02fcc219753b9dcb993a13fe83b0920
Julianne Bollain <143108399+JulianneB127@users.noreply.github.com>
2023-11-22 19:10:31 -0500

    enemy files

---
+++ EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehavior
+{
+	public int LifeTotoal = 1;
+	public int EnemySpeed = 20;
+	public Rigidbody rb;
+	
+	void Start()
+	{
+		rb = gameObjects.GetComponents<RigidBody2D>();
+	}
+
+	void Update()
+	{
+		rb.velocity = transform.right * EnemySpeed;
+	}
+	//must add rigid body and capsule collider to enemy sprit in developer
+
+}
---
+++ FlipEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipEnemy
+{
+	void OnTriggerEnter2D(collider2D Enemy)
+	{
+		if (Enemy.tag == "Enemy")
+		{
+			Enemy.transform.Rotate(0f, 180f, 0f);
+		}
+	}
+}
+//place on edges of enemy patrol area
